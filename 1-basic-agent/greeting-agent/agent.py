7eeb84c1eaf5d4fde20fab9e79fcaf604299ca46
nansuriyaa <suriyaaap13@gmail.com>
2025-10-26 16:01:51 +0530

    Implement agent with tools

--- 1-basic-agent/greeting-agent/agent.py
+++ 1-basic-agent/greeting-agent/agent.py
@@ -2,7 +2,6 @@ from google.adk.agents import Agent
 
 root_agent = Agent(
     name="greeting_agent",
-    # https://ai.google.dev/gemini-api/docs/models
     model="gemini-2.0-flash",
     description="Greeting agent",
     instruction="""
